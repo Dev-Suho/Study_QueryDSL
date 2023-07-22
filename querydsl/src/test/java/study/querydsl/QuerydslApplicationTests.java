6551e24ba5a9a0adb0e60c38d6c0d1c60e66361a
Dev-Suho <devsuho99@gmail.com>
2023-07-22 21:30:04 +0900

    EntityTest_0722

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,9 +1,15 @@
 package study.querydsl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
 
 	@Test
