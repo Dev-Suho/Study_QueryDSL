4e97d378b33874e0f57ea7f50885f4a6cb38f790
Dev-Suho <devsuho99@gmail.com>
2023-07-30 21:10:49 +0900

    QueryProjection_0730

--- querydsl/src/test/java/study/querydsl/QueryDslTest.java
+++ querydsl/src/test/java/study/querydsl/QueryDslTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -511,6 +512,17 @@ public class QueryDslTest {
         }
     }
 
+    @Test
+    void findByQueryProject() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
 
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+
+    }
 
 }
