41ea1e34f736b45e48715cc5c0b83de033043958
Dev-Suho <devsuho99@gmail.com>
2023-08-01 21:31:41 +0900

    RepositoryTest_0801

---
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,42 @@
+package study.querydsl.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void saveTest() {
+        Member member = new Member("memberA", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> memberList = memberJpaRepository.findAll();
+        assertThat(memberList).containsExactly(member);
+
+        List<Member> memberA = memberJpaRepository.findByUsername("memberA");
+        assertThat(memberA).containsExactly(member);
+    }
+}
