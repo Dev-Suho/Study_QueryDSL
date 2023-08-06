34a1c9cc903bf8c348bee797bc6dac4156c12ebf
Dev-Suho <devsuho99@gmail.com>
2023-08-06 21:18:31 +0900

    PredicateExecutor_0806

--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -67,4 +68,28 @@ class MemberRepositoryTest {
         assertThat(member).extracting("username").containsExactly("member4");
     }
 
+    @Test
+    void querydslPredicateExecutor() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 15, teamA);
+        Member member2 = new Member("member2", 25, teamA);
+        Member member3 = new Member("member3", 20, teamB);
+        Member member4 = new Member("member4", 35, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> memberList = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+
+        for (Member members : memberList) {
+            System.out.println("members = " + members);
+        }
+    }
 }
